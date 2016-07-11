8d8b293024d0d43c257aabbc6a89307635b82e81
Julius van der Vaart <studio@juliusvaart.com>
2016-07-11 16:51:45 +0200

    Updates

--- js/main.js
+++ js/main.js
@@ -1,12 +1,11 @@
 (function($) {
 
-
   $('.current').on('click', function() {
     
-    if($('.sitemenu').hasClass('open')) {
+    if($('#editooturbomenu').hasClass('open')) {
       
-      $('.sitemenu').animate({
-        marginTop: -140
+      $('#editooturbomenu').animate({
+        marginTop: '-12em'
       }, 300).removeClass('open');
       
       $('.current').text($textthing);
@@ -17,7 +16,7 @@
       
     } else {
     
-      $('.sitemenu').animate({
+      $('#editooturbomenu').animate({
         marginTop: 0
       }, 300).addClass('open');
       
@@ -33,65 +32,4 @@
     
   });
   
-  
-/*
-  $('.sitemenu').swipe({
-    
-    swipe:function(event, direction, distance, duration, fingerCount, fingerData) {
-      
-      if(direction == 'down' && distance > 10) {
-        
-        $('.sitemenu').animate({
-          marginTop: 0
-        }, 300).addClass('open');
-        
-      }
-      if(direction == 'up' && distance > 10) {
-        
-        $('.sitemenu').animate({
-          marginTop: -110
-        }, 300).removeClass('open');
-      
-      }
-      
-      //alert(distance);
-      
-      $('.text').text( phase +" you have swiped " + distance + "px in direction:" + direction );
-      
-    },
-    
-    allowPageScroll: 'vertical'
-    
-  });
-*/
-  
-/*
-  
-  $('.sitemenu').swipe( { swipeStatus:swipe2, allowPageScroll:"horizontal" } );
-  
-  function swipe2(event, phase, direction, distance) {
-    $('.text').text( phase +" you have swiped " + distance + "px in direction:" + direction );
-    
-    
-    
-    
-    if(direction == 'up') {
-      
-      $theswipe = distance + 110;
-    
-      $('.sitemenu').css({
-        marginTop: $theswipe
-      })
-    
-    }
-    
-    if(distance > 109) {
-      return false;
-    }
-    
-  }
-*/
-  
-  
-  
-})(jQuery);
+})(jQuery);  
